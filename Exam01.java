902dadc5feae9ea37fa16ea6342f577bd06df910
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-12-06 16:14:48 +0900

    MVC 패턴

---
+++ Exam01.java
@@ -0,0 +1,13 @@
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		//MVC 패턴 (Model View Controller)
+		//Model : 데이터들이 표현되는 역할.
+		//View : 보여지는 것. (사용자의 입장)
+		//Controller : 기능을 담당하는 역할
+		
+	}
+
+}
